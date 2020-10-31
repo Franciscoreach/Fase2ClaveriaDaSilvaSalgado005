5569e70d5a6b77afcd18e36534868c6b10dab0ee
Franciscoreach <francisco_claveria_aguila@hotmail.com>
2020-10-31 15:39:55 -0300

    MAEforo: Creando Modelos

--- MAEforo/contacto/models.py
+++ MAEforo/contacto/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 # Create your models here.
+class Entrada(models.Model):
+	
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='ID única para el caso de un Alumno.')
+	titulo = models.CharField(max_length=100)
+	tema = models.TextField(max_length=5000)
+
+	class Meta:
+		ordering = ['titulo']
+
+	def get_absolute_url(self):
+		return reverse('entrada-detail', args=[str(self.id)])
+
+	def __str__(self):
+		return f'Entrada: {self.titulo}'
